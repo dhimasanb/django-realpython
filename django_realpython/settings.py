5bee01ed247666478e74c9a5a16e70a9601ef34a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-06 05:23:09 +0700

    ganti database dan nama aplikasi

--- django_realpython/settings.py
+++ django_realpython/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'django_realpython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_realpython',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
